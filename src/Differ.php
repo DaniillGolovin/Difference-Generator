49d053011fb80444a284547235f096ebaec1477d
DaniillGolovin <daniilgolovin656@gmail.com>
2024-07-15 10:01:23 +0300

    type moved to variable

--- src/Differ.php
+++ src/Differ.php
@@ -16,66 +16,61 @@ function readFile(string $filePath): string
     return (string) file_get_contents($filePath);
 }
 
-function getType(string $path): string
-{
-    return pathinfo($path, PATHINFO_EXTENSION);
-}
-
 function genDiff(string $firstFilePath, string $secondFilePath, string $format = 'stylish'): string
 {
     $firstData = readFile($firstFilePath);
     $secondData = readFile($secondFilePath);
 
-    $parsedFirstData = parse($firstData, getType($firstFilePath));
-    $parsedSecondData = parse($secondData, getType($secondFilePath));
+    $parsedFirstData = parse($firstData, pathinfo($firstFilePath, PATHINFO_EXTENSION));
+    $parsedSecondData = parse($secondData, pathinfo($secondFilePath, PATHINFO_EXTENSION));
 
     $ast = makeNode($parsedFirstData, $parsedSecondData);
 
     return format($ast, $format);
 }
 
-function makeNode(array $firstDataArray, array $secondDataArray): array
+function makeNode(array $firstData, array $secondData): array
 {
-    $unionKeys = union(array_keys($firstDataArray), array_keys($secondDataArray));
+    $unionKeys = union(array_keys($firstData), array_keys($secondData));
     $sortedKeys = array_values(sortBy($unionKeys, fn($key) => $key));
 
-    $buildAst = array_map(function ($key) use ($firstDataArray, $secondDataArray): array {
-        if (!array_key_exists($key, $firstDataArray)) {
+    $buildAst = array_map(function ($key) use ($firstData, $secondData): array {
+        if (!array_key_exists($key, $firstData)) {
             return [
                 'key' => $key,
                 'type' => 'added',
                 'oldValue' => null,
-                'newValue' => $secondDataArray[$key],
+                'newValue' => $secondData[$key],
             ];
         }
-        if (!array_key_exists($key, $secondDataArray)) {
+        if (!array_key_exists($key, $secondData)) {
             return [
                 'key' => $key,
                 'type' => 'removed',
                 'oldValue' => null,
-                'newValue' => $firstDataArray[$key],
+                'newValue' => $firstData[$key],
             ];
         }
-        if (is_array($firstDataArray[$key]) && is_array($secondDataArray[$key])) {
+        if (is_array($firstData[$key]) && is_array($secondData[$key])) {
             return [
                 'key' => $key,
                 'type' => 'complex',
-                'children' => makeNode($firstDataArray[$key], $secondDataArray[$key]),
+                'children' => makeNode($firstData[$key], $secondData[$key]),
             ];
         }
-        if ($firstDataArray[$key] === $secondDataArray[$key]) {
+        if ($firstData[$key] === $secondData[$key]) {
             return [
                 'key' => $key,
                 'type' => 'unchanged',
-                'oldValue' => $firstDataArray[$key],
-                'newValue' => $secondDataArray[$key],
+                'oldValue' => $firstData[$key],
+                'newValue' => $secondData[$key],
             ];
         }
         return [
             'key' => $key,
             'type' => 'updated',
-            'oldValue' => $firstDataArray[$key],
-            'newValue' => $secondDataArray[$key],
+            'oldValue' => $firstData[$key],
+            'newValue' => $secondData[$key],
         ];
     }, $sortedKeys);
 
